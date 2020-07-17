647f82ec4985cd539a63595dd57ad9db83be126e
Angela Nguyen <angethuy@gmail.com>
2020-07-17 00:29:10 -0700

    feat(style): basic styling

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,24 @@ class MyApp extends StatelessWidget {
     final appTitle = 'Pagotometer';
 
     return MaterialApp(
-      title: appTitle,
-      home: PageContainer(title: appTitle),
-    );
+        title: appTitle,
+        home: PageContainer(title: appTitle),
+        theme: ThemeData(
+          // Define the default brightness and colors.
+          brightness: Brightness.dark,
+          primaryColor: Colors.lightBlue[800],
+          accentColor: Colors.cyan[600],
+
+          // Define the default font family.
+          fontFamily: 'Georgia',
+
+          // Define the default TextTheme. Use this to specify the default
+          // text styling for headlines, titles, bodies of text, and more.
+          textTheme: TextTheme(
+            headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+            headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+            bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+          ),
+        ));
   }
 }
